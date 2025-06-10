6f12fc5be8e3584b7cc1f43df3677d9759a1d035
Nifalu <n@nifalu.ch>
2025-06-10 13:16:29 +0200

    draw the graph

--- analyzer/coordinator.py
+++ analyzer/coordinator.py
@@ -1,3 +1,6 @@
+from schnauzer import \
+    VisualizationClient
+
 from analyzer.io_state import \
     IOConfig, \
     Component, \
@@ -14,37 +17,66 @@ class Coordinator:
     def __init__(self,
                  config_path: Path = Path.cwd() / "config.json",
                  ):
+        self.graph = None
         self.config = _parse(config_path)
-        self.graph = DiGraph()
 
     def run(self):
+        self.graph = DiGraph() # Reset the graph
 
         queue: list[IOSnapshot] = []
 
+        # Start analyzing the leaf components, i.e. components that do not depend on others.
         for cid in self.config.leaf_components:
-            arbitrary_input = IOState.unconstrained("input", 64)
+            arbitrary_input = IOState.unconstrained(f"input_{cid}", 64)
             c = self.config.components[cid]
             sa = SimpleAnalyzer(c.path, [arbitrary_input], self.config)
             snapshot = sa.analyze()
             snapshot.add_input(0, arbitrary_input)
             snapshot.print_rich()
             queue.append(snapshot)
+            self.graph.add_node(f"input_{cid}", type="input")
+            self.graph.add_node(snapshot.name, type="component")
+            self.graph.add_edge(f"input_{cid}", snapshot.name, type="symbolic")
 
         while queue:
             origin_snapshot = queue.pop(0)
             for cid, values in origin_snapshot.outputs.items():
                 if cid in self.config.leaf_components:
                     raise ValueError(f"Unexpected leaf component {cid} in queue. This should not happen.")
-                if cid == 0:
-                    continue # We have reached the root
+                if cid == 0:  # We have reached the root
+                    self.graph.add_node(f"output_{cid}", type="output")
+                    for v in values:
+                        t = "symbolic" if v.is_symbolic else "concrete"
+                        self.graph.add_edge(origin_snapshot.name, f"output_{cid}", type=t)
+                    continue
 
                 c = self.config.components[cid]
                 sa = SimpleAnalyzer(c.path, values, self.config)
                 new_snapshot = sa.analyze()
+                self.graph.add_node(new_snapshot.name, type="component")
                 for v in values:
                     new_snapshot.add_input(cid, v)
+                    t = "symbolic" if v.is_symbolic else "concrete"
+                    self.graph.add_edge(origin_snapshot.name, new_snapshot.name, type=t)
                 new_snapshot.print_rich()
                 queue.append(new_snapshot)
+
+        vc = VisualizationClient()
+        type_color_map = {
+            # Nodes
+            "input": "#9FE2BF",
+            "output": "#CCCCFF",
+            "component": "#6495ED",
+            # Edges
+            "symbolic": "#FFBF00",
+            "concrete": "#DE3163"
+        }
+        vc.send_graph(self.graph, type_color_map=type_color_map)
+
+
+
+
+
 
 
 
@@ -57,7 +89,6 @@ def _parse(path: Path) -> IOConfig:
     import json
     with open(path, 'r') as f:
         data = json.load(f)
-
     components_dir = Path(data['components_dir'])
     config = IOConfig({}, set())
     for comp in data['components']:
